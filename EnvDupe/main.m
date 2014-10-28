779aab07cc1a47a5c05baf796402906187c7b305
Jonathan McPherson <jonathan@rstudio.com>
2014-10-27 21:02:20 -0700

    Initial Commit

---
+++ EnvDupe/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  EnvDupe
+//
+//  Created by Jonathan McPherson on 10/27/14.
+//  Copyright (c) 2014 RStudio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+   return NSApplicationMain(argc, argv);
+}
